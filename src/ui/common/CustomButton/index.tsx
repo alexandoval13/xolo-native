6008237f596741df806809be4214574f92845b47
Alexandra Sandoval <alexandoval13@gmail.com>
2024-03-15 16:30:12 -0700

    Update some styling

--- src/ui/common/CustomButton/index.tsx
+++ src/ui/common/CustomButton/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-const styles = StyleSheet.create({
-  largeButtonContainer: {
-    margin: 16,
-    borderRadius: 8,
-    padding: 8,
-    height: 64,
-    width: 'auto',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'pink',
-  },
-  largeButtonText: {
-    color: 'white',
-    fontWeight: '600',
-    fontSize: 24,
-  },
-});
+const styles = (disabled: CustomButtonProps['disabled']) =>
+  StyleSheet.create({
+    largeButtonContainer: {
+      borderRadius: 4,
+      padding: 8,
+      height: 64,
+      width: 'auto',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: disabled ? 'gray' : 'pink',
+      opacity: disabled ? 0.5 : 1,
+    },
+    largeButtonText: {
+      color: 'white',
+      fontWeight: '600',
+      fontSize: 24,
+    },
+  });
 
 type CustomButtonTypes = 'large';
 type CustomButtonProps = {
@@ -32,11 +33,11 @@ const CustomButton = (props: CustomButtonProps) => {
   const {title, type, disabled, handlePress} = props;
 
   const containerClasses = {
-    large: styles.largeButtonContainer,
+    large: styles(disabled).largeButtonContainer,
   };
 
   const textClasses = {
-    large: styles.largeButtonText,
+    large: styles(disabled).largeButtonText,
   };
 
   return (
