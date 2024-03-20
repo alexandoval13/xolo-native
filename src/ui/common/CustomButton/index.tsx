6b13b1e06da1fe59253fd015c84dbdb78f7e5acd
Alexandra Sandoval <alexandoval13@gmail.com>
2024-03-19 17:50:24 -0700

    Adds styling to reusable custom btn

--- src/ui/common/CustomButton/index.tsx
+++ src/ui/common/CustomButton/index.tsx
@@ -1,50 +1,91 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-const styles = (disabled: CustomButtonProps['disabled']) =>
-  StyleSheet.create({
-    largeButtonContainer: {
+type StylesProps = {
+  isDisabled?: boolean;
+  backgroundColor?: string;
+};
+
+const largeButtonStyles = (props: StylesProps) => {
+  const {isDisabled, backgroundColor} = props;
+
+  return StyleSheet.create({
+    container: {
       borderRadius: 4,
       padding: 8,
-      height: 64,
-      width: 'auto',
       alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: disabled ? 'gray' : 'pink',
-      opacity: disabled ? 0.5 : 1,
+      alignSelf: 'center',
+      backgroundColor: isDisabled ? 'gray' : backgroundColor || 'pink',
+      opacity: isDisabled ? 0.5 : 1,
     },
-    largeButtonText: {
+    text: {
       color: 'white',
       fontWeight: '600',
       fontSize: 24,
     },
   });
+};
 
-type CustomButtonTypes = 'large';
+const iconButtonStyles = (props: StylesProps) => {
+  const {isDisabled, backgroundColor} = props;
+
+  return StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      padding: 8,
+      gap: 4,
+      alignSelf: 'center',
+      alignItems: 'center',
+      borderRadius: 4,
+      backgroundColor: backgroundColor || '',
+      opacity: isDisabled ? 0.5 : 1,
+    },
+    text: {
+      color: 'white',
+      fontSize: 12,
+    },
+  });
+};
+
+type CustomButtonTypes = 'large' | 'icon';
+
 type CustomButtonProps = {
-  title: string;
   type: CustomButtonTypes;
+  title?: string;
   disabled?: boolean;
   color?: string;
+  icon?: JSX.Element;
   handlePress: () => void;
 };
 
 const CustomButton = (props: CustomButtonProps) => {
-  const {title, type, disabled, handlePress} = props;
+  const {type, title, disabled, color, icon, handlePress} = props;
 
-  const containerClasses = {
-    large: styles(disabled).largeButtonContainer,
+  const getClasses = ({isDisabled, backgroundColor}: StylesProps) => {
+    switch (type) {
+      case 'large':
+        return largeButtonStyles({isDisabled, backgroundColor});
+      case 'icon':
+        return iconButtonStyles({isDisabled, backgroundColor});
+    }
   };
 
-  const textClasses = {
-    large: styles(disabled).largeButtonText,
-  };
+  const classes = getClasses({isDisabled: disabled, backgroundColor: color});
 
   return (
     <TouchableOpacity onPress={handlePress} disabled={disabled}>
-      <View style={containerClasses[type]}>
-        <Text style={textClasses[type]}>{title}</Text>
-      </View>
+      {type === 'large' && (
+        <View style={classes.container}>
+          {title && <Text style={classes.text}>{title}</Text>}
+          {icon && icon}
+        </View>
+      )}
+      {type === 'icon' && (
+        <View style={classes.container}>
+          {title && <Text style={classes.text}>{title}</Text>}
+          {icon && icon}
+        </View>
+      )}
     </TouchableOpacity>
   );
 };
