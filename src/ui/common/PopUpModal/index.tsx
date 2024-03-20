51f1b636742b217a6c751e8a9b96c72a9c9da46c
Alexandra Sandoval <alexandoval13@gmail.com>
2024-03-19 19:51:06 -0700

    Adds styling and functionality to PopUpModal

--- src/ui/common/PopUpModal/index.tsx
+++ src/ui/common/PopUpModal/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import {
   Modal,
+  ScrollView,
   StyleSheet,
   TouchableOpacity,
   TouchableWithoutFeedback,
+  View,
 } from 'react-native';
+import CustomButton from '../CustomButton';
+import CloseIcon from '../../../assets/mui/CloseIcon';
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
     justifyContent: 'flex-end',
     shadowOpacity: 0.2,
     shadowOffset: {height: -5, width: 0},
     shadowRadius: 20,
+  },
+  container: {
+    height: 360,
+    padding: 24,
+    borderTopStartRadius: 4,
+    borderTopEndRadius: 4,
+    backgroundColor: 'pink',
+  },
+  closeButton: {
+    position: 'absolute',
+    right: 8,
+    top: 8,
   },
 });
 
@@ -20,14 +36,33 @@ type PopUpModalProps = {
   visible: boolean;
   handleClose: () => void;
   children: any;
+  closeButton?: boolean;
 };
 
-const PopUpModal = ({visible, handleClose, children}: PopUpModalProps) => {
+const PopUpModal = ({
+  visible,
+  handleClose,
+  children,
+  closeButton,
+}: PopUpModalProps) => {
   const classes = styles;
   return (
     <Modal visible={visible} animationType="slide" transparent>
-      <TouchableOpacity style={classes.container} onPressOut={handleClose}>
-        <TouchableWithoutFeedback>{children}</TouchableWithoutFeedback>
+      <TouchableOpacity style={classes.main} onPressOut={handleClose}>
+        <TouchableWithoutFeedback>
+          <View style={classes.container}>
+            {closeButton && (
+              <View style={classes.closeButton}>
+                <CustomButton
+                  type="icon"
+                  icon={<CloseIcon height={16} width={16} />}
+                  handlePress={handleClose}
+                />
+              </View>
+            )}
+            <ScrollView>{children}</ScrollView>
+          </View>
+        </TouchableWithoutFeedback>
       </TouchableOpacity>
     </Modal>
   );
